98caa0165f5910c65ac9350a70327acc1c9e1148
sean <bih00425@gmail.com>
2023-07-09 22:56:35 +0800

    tmp

--- app/routes/cal-sellout.jsx
+++ app/routes/cal-sellout.jsx
@@ -2,6 +2,26 @@ import { ClientOnly } from "remix-utils";
 
 import CalSellout from '~/components/CalSellout/index.client'
 
+import { PrismaClient } from '@prisma/client';
+ 
+const prisma = new PrismaClient();
+ 
+export const prismaExample = async () => {
+//  const newUser = await prisma.TransactionsType.create({
+//    data: {
+//      name: '買',
+//    },
+//  });
+ 
+  const users = await prisma.TransactionsType.findMany();
+  console.log(users)
+  return null
+}
+
+export const loader = async () => {
+  await prismaExample()
+}
+
 const CalSelloutRoute = () => {
   return (
     <>
