bf290dde16532bdd68bc7b88a2ed9a432ee73833
sean <bih00425@gmail.com>
2023-07-10 23:08:19 +0800

    update filter stock title

--- app/components/FilterStock/components/TitleField.js
+++ app/components/FilterStock/components/TitleField.js
@@ -10,10 +10,11 @@ const TitleField = ({ onChange }) => {
       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an title</label>
       <select id="title" onChange={handleChange} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option selected value=""></option>
-        <option value="激戰區-百元">激戰區-百元</option>
-        <option value="激戰區-非百元">激戰區-非百元</option>
+        <option value="激戰區-非百元(Tick 適用)">激戰區-非百元(Tick 適用)</option>
+        <option value="激戰區-百元(Tick 適用)">激戰區-百元(Tick 適用)</option>
         <option value="上班族首選-多排">上班族首選-多排</option>
         <option value="成長型-價投">成長型-價投</option>
+        <option value="型態首選-動能追追">型態首選-動能追追</option>
       </select>
     </>
   )
